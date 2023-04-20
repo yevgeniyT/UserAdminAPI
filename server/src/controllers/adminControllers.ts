983bf10e1be79230fd433724f3cdb2aec3a7e7f2
yevgeniyT <yevgentolkachov@gmail.com>
2023-04-21 00:02:27 +0200

    update,delete added

--- server/src/controllers/adminControllers.ts
+++ server/src/controllers/adminControllers.ts
@@ -24,6 +24,7 @@ const getAllUsers = async (req: Request, res: Response) => {
 };
 const getUserById = async (req: Request, res: Response) => {
     try {
+        //Check if the user exist
         const user = await User.findById(req.params.userId);
         if (!user) {
             return res.status(404).json({
@@ -47,8 +48,13 @@ const getUserById = async (req: Request, res: Response) => {
 };
 const createUser = async (req: Request, res: Response) => {
     try {
-        // Your implementation logic goes here
-        // For example: Fetch data, update data, delete data, etc.
+        //Check if the user exist
+        const user = await User.findById(req.body.email);
+        if (!user) {
+            return res.status(404).json({
+                message: "Can not find user with such id",
+            });
+        }
 
         res.status(200).json({
             message: "Successful operation",
@@ -67,12 +73,30 @@ const createUser = async (req: Request, res: Response) => {
 };
 const updateUserById = async (req: Request, res: Response) => {
     try {
-        // Your implementation logic goes here
-        // For example: Fetch data, update data, delete data, etc.
+        //Check if the user exist
+        const userId = req.params.userId;
+        const { firstName, lastName, role, isActive }: userType = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: "Can not find user with such id",
+            });
+        }
 
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { firstName, lastName, role, isActive },
+            //new: true: By default, the findByIdAndUpdate method returns the original document before the update. When you set the new option to true, it will return the updated document instead.
+            //runValidators: true: Mongoose schemas can have validation rules defined, such as required fields, minlength, maxlength, etc. By default, these validation rules are applied when creating new documents, but not when updating existing ones. Setting the runValidators option to true ensures that the update operation follows the schema validation rules, so any updates that don't meet the criteria will result in an error.
+            { new: true, runValidators: true }
+        );
+        if (!updatedUser) {
+            return res.status(500).json({
+                message: "An error occurred while updating the user profile.",
+            });
+        }
         res.status(200).json({
-            message: "Successful operation",
-            // Add other relevant response data
+            message: "User has been updated sucessfuly",
         });
     } catch (error: unknown) {
         if (typeof error === "string") {
@@ -87,12 +111,15 @@ const updateUserById = async (req: Request, res: Response) => {
 };
 const deleteUserById = async (req: Request, res: Response) => {
     try {
-        // Your implementation logic goes here
-        // For example: Fetch data, update data, delete data, etc.
-
+        const user = await User.findById(req.params.userId);
+        if (!user) {
+            return res.status(404).json({
+                message: "Can not find user with such id",
+            });
+        }
+        await User.findByIdAndDelete(req.params.userId);
         res.status(200).json({
-            message: "Successful operation",
-            // Add other relevant response data
+            message: "User was deleted sucessfuly",
         });
     } catch (error: unknown) {
         if (typeof error === "string") {
--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -16,5 +16,6 @@ const adminRouter = Router();
 //This route will first check if the user is logged in using the isLoggedIn middleware. If the user is logged in, the isAdmin middleware will then check if the user has an admin role. If the user is an admin, the getAllUsers controller function will be executed, and the list of all users will be sent as a response.
 adminRouter.get("/users", isLoggedIn, isAdmin, getAllUsers);
 adminRouter.get("/:userId", isLoggedIn, isAdmin, getUserById);
-
+adminRouter.put("/:userId", isLoggedIn, isAdmin, updateUserById);
+adminRouter.delete("/:userId", isLoggedIn, isAdmin, deleteUserById);
 export default adminRouter;
